dfd5baf056f57470d3157e2beed1a93d85d55b0e
Alain Achkar <rails@alain.simplytech.com>
2013-07-28 20:39:15 -0400

    Initial commit ruby-2.0.0-p195@my_shoppify_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyShopifyApp::Application.config.secret_key_base = '7d6327dba6c455f9884cbf67fe27a659d28ff66adcdb36f009100939ac077941d5b8f640baa5800436df71e15569247cef616a8d0488426c419e0fcbf3cdfb48'
